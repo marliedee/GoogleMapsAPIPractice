8c5bf60f0e82488dc8cede419a84a26900973a61
marliedee <marlyd364@gmail.com>
2019-02-03 17:39:35 -0500

    first commit

---
+++ app/src/main/java/org/pursuit/myapplicationmaps/MainActivity.java
@@ -0,0 +1,25 @@
+package org.pursuit.myapplicationmaps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.map_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
